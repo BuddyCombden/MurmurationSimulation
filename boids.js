fb0bfa73c6b988846d4cc34c91b00ef208fdc35d
BuddyCombden <49070343+BuddyCombden@users.noreply.github.com>
2020-04-30 08:45:39 -0400

    2.0 Correction

--- boids.js
+++ boids.js
@@ -1,6 +1,10 @@
+/*
+Original base code acquired from Ben Eater @ https://github.com/beneater/boids
+*/
+
 // Size of canvas. These get updated to fill the whole browser.
-let width = 1;
-let height = 1;
+let width = 150;
+let height = 150;
 
 //Program Amplifier
 const progScale = 1;
@@ -8,14 +12,34 @@ const progScale = 1;
 const simScaling = 1*progScale;
 //Simulation Speed Factor
 const simSpeed = 1*progScale;
+//Number of Boid Types
+const numTypes = 2;
 
 //Auto adjusts number of boids and their range to better fit scaling.
-const numBoids = simScaling*200;
+let numBoids = 1;
 const visualRange = 75//+(numBoids*0.15);
+const x_margin = 200//window.innerWidth*0.15;//Margin from sides of screen
+const y_margin = 120//window.innerHeight*0.2;//Mergin from top/bottom of screen
 
 //What percentage of the tail should persist | 0->100%
-const tailPortion = 50;
+const tailPortion = 10;
 const DRAW_TRAIL = false;
+
+//Normal Boid Traversal
+const centeringFactor = 0.005*simSpeed; // adjust velocity by this %
+const minDistance = 20; // The distance to stay away from other boids
+const avoidFactor = 0.05*simSpeed; // Adjust velocity by this %
+const matchingFactor = 0.05*simSpeed; // Adjust by this % of average velocity
+const speedLimit = 10*simSpeed;//Boid topspeed
+const turnFactor = 1*simSpeed;//Boid turnspeed
+
+const boidColors = ["#282c34d1","#345258d1","#3f353ea1","#6f4472"]
+const trailColors = ["#282c3436","#34525836","#3f353e36","#6f447236"]
+const rainbow = {
+	r: 0,
+	g: 0,
+	b: 100,
+	max: 100};
 
 var boids = [];
 
@@ -26,13 +50,17 @@ function logStates(){
 }
 
 function initBoids() {
+  numBoids = Math.round((width*height)/(6000*(simScaling/2)));
+  console.log("NumBoids: "+numBoids);
   for (var i = 0; i < numBoids; i += 1) {
+		//console.log(numTypes-1-Math.floor((Math.log(boids.length+1)/Math.log(numBoids))/(1-(Math.log(numTypes)/Math.log(6)))));
     boids[boids.length] = {
       x: Math.random() * width,
       y: Math.random() * height,
       dx: Math.random() * 10 - 5,
       dy: Math.random() * 10 - 5,
       history: [],
+	  type: (numTypes-1-Math.floor((Math.log(boids.length+1)/Math.log(numBoids))/(1-(Math.log(numTypes)/Math.log(6))))),
     };
   }
 }
@@ -60,130 +88,159 @@ function sizeCanvas() {
   const canvas = document.getElementById("boids");
   width = window.innerWidth*simScaling;
   height = window.innerHeight*simScaling;
+  console.log("Width: "+width);
+  console.log("Height: "+height);
   canvas.width = width;
   canvas.height = height;
 }
 
-// Constrain a boid to within the window. If it gets too close to an edge,
-// nudge it back in and reverse its direction.
-function keepWithinBounds(boid) {
-  const x_margin = 200;
-  const y_margin = 120;
-  const turnFactor = 1*simSpeed;
-
-  if (boid.x < x_margin) {
-    boid.dx += turnFactor;
-  }
-  if (boid.x > width - x_margin) {
-    boid.dx -= turnFactor
-  }
-  if (boid.y < y_margin) {
-    boid.dy += turnFactor;
-  }
-  if (boid.y > height - y_margin) {
-    boid.dy -= turnFactor;
-  }
+function shiftRainbow(color){
+	//console.log("rgb("+color.r+","+color.g+","+color.b+",255) "+color.state);
+	if(color.r == color.max){
+		if(color.g == color.max){color.r -= 1;}
+		else if(color.b > 0){color.b -=1;}
+		else{color.g += 1;}}
+	if(color.g == color.max){
+		if(color.b == color.max){color.g -= 1;}
+		else if(color.r > 0){color.r -=1;}
+		else{color.b += 1;}}
+	if(color.b == color.max){
+		if(color.r == color.max){color.b -= 1;}
+		else if(color.g > 0){color.g -=1;}
+		else{color.r += 1;}}
 }
 
-// Find the center of mass of the other boids and adjust velocity slightly to
-// point towards the center of mass.
-function flyTowardsCenter(boid) {
-  const centeringFactor = 0.005*simSpeed; // adjust velocity by this %
+function boidBehaviour(boid){
+	let centerX = 0;
+	let centerY = 0;
+	let moveX = 0;
+	let moveY = 0;
+	let avgDX = 0;
+	let avgDY = 0;
+	let numNeighbors = 0;
+	let boidDist = 0;
+	let sameType = false;
+	let typeDiff = 0;
+	for (let otherBoid of boids) {
+		boidDist = distance(boid,otherBoid);
+		sameType = (boid.type == otherBoid.type);
+		typeDiff = otherBoid.type - boid.type;
+		if(boidDist < visualRange){
+	if(sameType){
+				centerX += otherBoid.x;
+				centerY += otherBoid.y;
+				avgDX += otherBoid.dx;
+				avgDY += otherBoid.dy;
+				numNeighbors += 1;
+		}}
+		if (otherBoid !== boid) {
+			if(boidDist < minDistance){
+				if(typeDiff > 0){
+					moveX += (boid.x - otherBoid.x)*(typeDiff*typeDiff*0.5+0.35);
+					moveY += (boid.y - otherBoid.y)*(typeDiff*typeDiff*0.5+0.35);
+				}
+				if(sameType){
+					moveX += boid.x - otherBoid.x;
+					moveY += boid.y - otherBoid.y;
+				}
 
-  let centerX = 0;
-  let centerY = 0;
-  let numNeighbors = 0;
+		}}
+	}
+	if (numNeighbors) {
+		centerX = centerX / numNeighbors;
+		centerY = centerY / numNeighbors;
+		avgDX = avgDX / numNeighbors;
+		avgDY = avgDY / numNeighbors;
 
-  for (let otherBoid of boids) {
-    if (distance(boid, otherBoid) < visualRange) {
-      centerX += otherBoid.x;
-      centerY += otherBoid.y;
-      numNeighbors += 1;
-    }
-  }
-
-  if (numNeighbors) {
-    centerX = centerX / numNeighbors;
-    centerY = centerY / numNeighbors;
-
-    boid.dx += (centerX - boid.x) * centeringFactor;
-    boid.dy += (centerY - boid.y) * centeringFactor;
-  }
-}
-
-// Move away from other boids that are too close to avoid colliding
-function avoidOthers(boid) {
-  const minDistance = 20; // The distance to stay away from other boids
-  const avoidFactor = 0.05*simSpeed; // Adjust velocity by this %
-  let moveX = 0;
-  let moveY = 0;
-  for (let otherBoid of boids) {
-    if (otherBoid !== boid) {
-      if (distance(boid, otherBoid) < minDistance) {
-        moveX += boid.x - otherBoid.x;
-        moveY += boid.y - otherBoid.y;
-      }
-    }
-  }
-
-  boid.dx += moveX * avoidFactor;
-  boid.dy += moveY * avoidFactor;
-}
-
-// Find the average velocity (speed and direction) of the other boids and
-// adjust velocity slightly to match.
-function matchVelocity(boid) {
-  const matchingFactor = 0.05*simSpeed; // Adjust by this % of average velocity
-
-  let avgDX = 0;
-  let avgDY = 0;
-  let numNeighbors = 0;
-
-  for (let otherBoid of boids) {
-    if (distance(boid, otherBoid) < visualRange) {
-      avgDX += otherBoid.dx;
-      avgDY += otherBoid.dy;
-      numNeighbors += 1;
-    }
-  }
-
-  if (numNeighbors) {
-    avgDX = avgDX / numNeighbors;
-    avgDY = avgDY / numNeighbors;
-
-    boid.dx += (avgDX - boid.dx) * matchingFactor;
-    boid.dy += (avgDY - boid.dy) * matchingFactor;
-  }
-}
-
-// Speed will naturally vary in flocking behavior, but real animals can't go
-// arbitrarily fast.
-function limitSpeed(boid) {
-  const speedLimit = 15*simSpeed;
-
-  const speed = Math.sqrt(boid.dx * boid.dx + boid.dy * boid.dy);
-  if (speed > speedLimit) {
-    boid.dx = (boid.dx / speed) * speedLimit;
-    boid.dy = (boid.dy / speed) * speedLimit;
-  }
+		boid.dx += ((avgDX - boid.dx) * matchingFactor) + ((centerX - boid.x) * centeringFactor);
+		boid.dy += ((avgDY - boid.dy) * matchingFactor) + ((centerY - boid.y) * centeringFactor);
+	}
+	boid.dx += moveX * avoidFactor;
+	boid.dy += moveY * avoidFactor;
+	//Speed Limiting
+	const speed = Math.sqrt(boid.dx * boid.dx + boid.dy * boid.dy);
+	if (speed > speedLimit/((boid.type+2)/2)) {
+		boid.dx = (boid.dx / speed) * speedLimit;
+		boid.dy = (boid.dy / speed) * speedLimit;
+	}
+	//Bound Constraints
+	if (boid.x < x_margin) {
+		boid.dx += turnFactor;
+	}
+	if (boid.x > width - x_margin) {
+		boid.dx -= turnFactor;
+	}
+	if (boid.y < y_margin) {
+		boid.dy += turnFactor;
+	}
+	if (boid.y > height - y_margin) {
+		boid.dy -= turnFactor;
+	}
 }
 
 function drawBoid(ctx, boid) {
   const angle = Math.atan2(boid.dy, boid.dx);
-  ctx.translate(boid.x, boid.y);
-  ctx.rotate(angle);
-  ctx.translate(-boid.x, -boid.y);
-  ctx.fillStyle = "#558cf4";
-  ctx.beginPath();
-  ctx.moveTo(boid.x, boid.y);
-  ctx.lineTo(boid.x - 15, boid.y + 5);
-  ctx.lineTo(boid.x - 15, boid.y - 5);
-  ctx.lineTo(boid.x, boid.y);
+	  ctx.translate(boid.x, boid.y);
+	  ctx.rotate(angle);
+	  ctx.translate(-boid.x, -boid.y);
+	  ctx.beginPath();
+	  ctx.moveTo(boid.x, boid.y);
+	  ctx.fillStyle = boidColors[boid.type];
+  if(boid.type == 0){
+		ctx.lineTo(boid.x + 6, boid.y);
+		ctx.lineTo(boid.x - 6, boid.y + 4);
+		ctx.lineTo(boid.x-10, boid.y);
+		ctx.lineTo(boid.x - 6, boid.y - 4);
+		ctx.lineTo(boid.x + 6, boid.y);
+  }
+  else if(boid.type == 1){
+		ctx.lineTo(boid.x + 10, boid.y);
+		ctx.lineTo(boid.x - 10, boid.y+6);
+		ctx.lineTo(boid.x - 5, boid.y);
+		ctx.lineTo(boid.x - 10, boid.y-6);
+		ctx.lineTo(boid.x + 10, boid.y);
+  }
+  else if(boid.type == 2){
+		ctx.lineTo(boid.x + 16, boid.y);
+		ctx.lineTo(boid.x + 8, boid.y+3);
+		ctx.lineTo(boid.x + 3, boid.y+7);
+		ctx.lineTo(boid.x - 9, boid.y+12);
+		ctx.lineTo(boid.x - 3, boid.y+3);
+		ctx.lineTo(boid.x - 16, boid.y+5);
+		ctx.lineTo(boid.x - 8, boid.y);
+		ctx.lineTo(boid.x - 16, boid.y-5);
+		ctx.lineTo(boid.x - 3, boid.y-3);
+		ctx.lineTo(boid.x - 9, boid.y-12);
+		ctx.lineTo(boid.x + 3, boid.y-7);
+		ctx.lineTo(boid.x + 8, boid.y-3);
+		ctx.lineTo(boid.x + 16, boid.y);
+  }
+  else if(boid.type == 3){
+		ctx.lineTo(boid.x - 35, boid.y + 10);
+		ctx.lineTo(boid.x - 35, boid.y - 10);
+		ctx.lineTo(boid.x, boid.y);
+		ctx.fill();
+		ctx.fillStyle = "#633F15b1"
+		ctx.lineTo(boid.x + 10, boid.y);
+		ctx.lineTo(boid.x - 20, boid.y + 10);
+		ctx.lineTo(boid.x - 20, boid.y - 10);
+		ctx.lineTo(boid.x+10, boid.y);
+  }
+  else{
+		ctx.fillStyle = "#cacadaaa"
+		//ctx.fillStyle = "rgb("+rainbow.r+","+rainbow.g+","+rainbow.b+",255)";
+		//console.log("rgb("+bgTest[0]+","+bgTest[1]+","+bgTest[2]+")");
+    ctx.lineTo(boid.x + 10, boid.y);
+    ctx.lineTo(boid.x, boid.y + 5);
+    ctx.lineTo(boid.x - 14, boid.y);
+		ctx.lineTo(boid.x, boid.y - 5);
+		ctx.lineTo(boid.x + 10, boid.y);
+  }
   ctx.fill();
   ctx.setTransform(1/simScaling, 0, 0, 1/simScaling, 0, 0);
 
   if (DRAW_TRAIL) {
-    ctx.strokeStyle = "#558cf436";
+    ctx.strokeStyle = trailColors[boid.type];
     ctx.beginPath();
     ctx.moveTo(boid.history[0][0], boid.history[0][1]);
     for (const point of boid.history) {
@@ -198,22 +255,26 @@ function animationLoop() {
   // Update each boid
   for (let boid of boids) {
 	// Update the velocities according to each rule
-	flyTowardsCenter(boid);
+	/*flyTowardsCenter(boid);
 	avoidOthers(boid);
 	matchVelocity(boid);
 	limitSpeed(boid);
-	keepWithinBounds(boid);
+	keepWithinBounds(boid);*/
+	boidBehaviour(boid);
 
 	// Update the position based on the current velocity
 	boid.x += boid.dx;
 	boid.y += boid.dy;
-	boid.history.push([boid.x, boid.y])
-	boid.history = boid.history.slice(-tailPortion);
+	if(DRAW_TRAIL){
+		boid.history.push([boid.x, boid.y])
+		boid.history = boid.history.slice(-tailPortion);
+	}
   }
 
   // Clear the canvas and redraw all the boids in their current positions
   const ctx = document.getElementById("boids").getContext("2d");
   ctx.clearRect(0, 0, width, height);
+	shiftRainbow(rainbow);
   for (let boid of boids) {
 	drawBoid(ctx, boid);
   }
@@ -226,10 +287,9 @@ window.onload = () => {
   // Make sure the canvas always fills the whole window
   window.addEventListener("resize", sizeCanvas, false);
   sizeCanvas();
-  
+
   //Creates console logs of the current program states.
   logStates();
-  
   // Randomly distribute the boids to start
   initBoids();
 
